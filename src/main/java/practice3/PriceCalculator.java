17b30953f0c2d36676e49bac5d27801d9cef1c03
ooxiaoyan <810978527@qq.com>
2018-04-18 01:43:46 +0800

    Refactoring PriceCalculator.java

--- src/main/java/practice3/PriceCalculator.java
+++ src/main/java/practice3/PriceCalculator.java
@@ -1,4 +1,34 @@
 package practice3;
 
-public class PriceCaculator {
+import java.math.BigDecimal;
+
+public class PriceCalculator {
+
+    private Order order;
+
+    public PriceCalculator(Order order) {
+        this.order = order;
+    }
+
+    public BigDecimal calculate() {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        // Total up line items
+        for (OrderLineItem lineItem : order.getOrderLineItemList()) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        // Subtract discounts
+        for (BigDecimal discount : order.getDiscounts()) {
+            subTotal = subTotal.subtract(discount);
+        }
+
+        // calculate tax
+        BigDecimal tax = subTotal.multiply(order.getTax());
+
+        // calculate GrandTotal
+        BigDecimal grandTotal = subTotal.add(tax);
+
+        return grandTotal;
+    }
 }
